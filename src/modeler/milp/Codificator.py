61ab709e3c6fd0fd0e1cfb85735706e424ad6ca5
Luiz Fernando <luizfernando.lf355@gmail.com>
2024-09-12 08:19:55 -0300

    Feat: Inicalização da nova abordagem

---
+++ src/modeler/milp/Codificator.py
@@ -0,0 +1,132 @@
+import numpy as np
+from cplex import infinity
+
+from docplex.mp.model import Model
+
+
+class Codificator:
+
+    def __init__(self, network, dataframe):
+        self.network = network
+        self.dataframe = dataframe
+        self.milp_represetation = None
+
+        self.input_types = None
+        self.bounds_large = None
+
+        self.input_variables = None
+        self.intermediate_variables = None
+        self.decision_variables = None
+        self.output_variables = None
+
+    def codify_network_milp_large_bounds(self):
+        self.bounds_large = []
+        self.bounds_large.append(self.get_types_and_bounds())
+
+        self.milp_represetation = Model()
+
+        self._init_input_variables()
+
+        self._init_intermediate_variables()
+
+        self.output_variables = self.milp_represetation.continuous_var_list(self.network.layers[-1].weight.detach().numpy().T.shape[1],
+                                                                            lb=-infinity,
+                                                                            name='o'
+                                                                            )
+
+        self._codify_tjeng()
+
+        return self.milp_represetation
+
+    def _codify_tjeng(self):
+        len_layers = len(self.network.layers)
+        for i in range(len_layers):
+            A = self.network.layers[i].weight.detach().numpy()
+
+            b = self.network.layers[i].bias.detach().numpy()
+
+            x = self.input_variables if i == 0 else self.intermediate_variables[i - 1]
+
+            bounds = []
+
+            if i != (len_layers - 1):
+                a = self.decision_variables[i]
+                y = self.intermediate_variables[i]
+            else:
+                y = self.output_variables
+
+            for j in range(A.shape[0]):
+                weighted_sum = A[j, :] @ x + b[j]
+
+                ub = self._maximize(weighted_sum)
+                lb = self._minimize(weighted_sum)
+
+                if i != len_layers - 1:
+                    self.milp_represetation.add_constraint(y[j] <= weighted_sum - lb * (1 - a[j]))
+                    self.milp_represetation.add_constraint(y[j] >= weighted_sum)
+                    self.milp_represetation.add_constraint(y[j] <= ub * a[j])
+
+                    bounds.append((lb, ub))
+
+                else:
+                    self.milp_represetation.add_constraint(weighted_sum == y[j])
+
+                    bounds.append((lb, ub))
+            self.bounds_large.append(bounds)
+        return self.milp_represetation
+
+    def _maximize(self, expr):
+        self.milp_represetation.maximize(expr)
+        self.milp_represetation.solve()
+        ub = self.milp_represetation.solution.get_objective_value()
+        self.milp_represetation.remove_objective()
+        return ub
+
+    def _minimize(self, expr):
+        self.milp_represetation.minimize(expr)
+        self.milp_represetation.solve()
+        lb = self.milp_represetation.solution.get_objective_value()
+        self.milp_represetation.remove_objective()
+        return lb
+
+    def _init_input_variables(self):
+        self.input_variables = []
+        for index, (domain_type, bounds) in enumerate(zip(self.input_types, self.bounds_large[0])):
+            lower_bound, upper_bound = bounds
+            name = f'x_{index}'
+            if domain_type == 'C':
+                self.input_variables.append(self.milp_represetation.continuous_var(lb=lower_bound, ub=upper_bound, name=name))
+            elif domain_type == 'I':
+                self.input_variables.append(self.milp_represetation.integer_var(lb=lower_bound, ub=upper_bound, name=name))
+            else:
+                self.input_variables.append(self.milp_represetation.binary_var(name=name))
+
+    def _init_intermediate_variables(self):
+        self.intermediate_variables = []
+        self.decision_variables = []
+        for idx_layer in range(len(self.network.layers) - 1):
+            len_neurons = self.network.layers[idx_layer].weight.detach().numpy().T.shape[1]
+
+            self.intermediate_variables.append(self.milp_represetation.continuous_var_list(len_neurons,
+                                                                                  lb=0, # -infinity PARA OBTER MAIORES BOUNDS
+                                                                                  name='y',
+                                                                                  key_format=f"_{idx_layer}_%s"))
+
+            self.decision_variables.append(self.milp_represetation.binary_var_list(len_neurons,
+                                                                          name='a',
+                                                                          lb=0,
+                                                                          ub=1,
+                                                                          key_format=f"_{idx_layer}_%s"))
+
+    def get_types_and_bounds(self):
+        self.input_types = []
+        columns = self.dataframe.columns
+        bounds = []
+        for column in columns:
+            unique_values = self.dataframe[column].unique()
+            self.input_types.append(
+                'B' if len(unique_values) == 2 else
+                'C' if np.any(unique_values.astype(np.int64) != unique_values.astype(np.float64)) else
+                'I')
+            bounds.append((self.dataframe[column].min(), self.dataframe[column].max()))
+        return bounds
---
+++ src/modeler/milp/Explanator.py
@@ -0,0 +1,150 @@
+from collections import deque
+
+import docplex.mp.model as mp
+import pandas as pd
+
+from src.modeler.milp.Codificator import Codificator
+from src.modeler.network.NeuralNetwork import NeuralNetwork
+
+
+class Explanator:
+    def __init__(self, network: NeuralNetwork, dataframe: pd.DataFrame, input_tensors):
+        self.network = network
+        self.dataframe = dataframe
+        self.input_tensors = input_tensors
+        dataframe.drop(columns='target', inplace=True)
+        self.codificator = Codificator(network, dataframe)
+
+    def back_explication(self, instance_index):
+        self.codificator.codify_network_milp_large_bounds()
+
+        bounds = deque()
+
+        bounds.appendleft(self._explain_last_layer(instance_index))
+
+        return bounds
+
+
+    def _explain_last_layer(self, instance_index):
+        index_output_network = self.network.forward(self.input_tensors[instance_index]).argmax().item()
+        input_values = self.network.get_layer_values(self.input_tensors[instance_index],
+                                                     self.network.len_layers - 2
+                                                     ).tolist()
+        weights = self.network.layers[-1].weight.detach().numpy()
+        bias = self.network.layers[-1].bias.detach().numpy()
+
+        milp = mp.Model()
+
+        input_vars = []
+        input_constraints = []
+
+        output_vars = []
+        weighted_sum_constraints = []
+
+        for j_neuron, bound in enumerate(self.codificator.bounds_large[-2]):
+            lb, ub = bound
+            input_vars.append(milp.continuous_var(lb=lb, ub=ub, name=f'in_{j_neuron}'))
+            input_constraints.append(milp.add_constraint(input_vars[j_neuron] == input_values[j_neuron],
+                                                         ctname=f'input_{j_neuron}'
+                                                         ))
+
+        for j_neuron, bound in enumerate(self.codificator.bounds_large[-1]):
+            lb, ub = bound
+            output_vars.append(milp.continuous_var(lb=lb, ub=ub, name=f'out_{j_neuron}'))
+            weighted_sum_constraints.append(
+                milp.add_constraint(output_vars[j_neuron] == weights[j_neuron, :] @ input_vars + bias[j_neuron],
+                                    ctname=f'weighted_sum_{j_neuron}'
+                                    ))
+
+        bounds = []
+        lb_j_plus_e = ub_j_minus_e = float()
+        for j in range(len(input_values)):
+            milp.remove_constraint(input_constraints[j])
+
+            if input_vars[j].ub <= 0:
+                bounds.append((input_vars[j].lb, input_vars[j].ub))
+                milp.add_constraint(input_constraints[j])
+                continue
+
+            for o in range(self.network.list_len_neurons[-1]):
+                to_min_constraint = milp.add_constraint(input_values[j] <= input_vars[j])
+                if o == index_output_network:
+                    continue
+
+                # add
+                output_constraint = milp.add_constraint( output_vars[o] >= output_vars[index_output_network],
+                                                         ctname=f'O_i>=O_j'
+                                                         )
+
+                # solve
+                milp.minimize(input_vars[j])
+                milp.solve()
+                milp.remove_objective()
+
+                if milp.solution is None:
+                    ub_j_minus_e = input_vars[j].ub
+                else:
+                    minimized_x_j = milp.solution.get_objective_value()
+                    ub_j_minus_e = minimized_x_j - (minimized_x_j - input_values[j]) * 0.01
+                    input_vars[j].ub = ub_j_minus_e
+
+                milp.remove_constraint(to_min_constraint)
+
+                milp.maximize(input_vars[j])
+                milp.solve()
+                milp.remove_objective()
+                milp.remove_constraint(output_constraint)
+
+                if milp.solution is None:
+                    lb_j_plus_e = input_vars[j].lb
+                    continue
+                else:
+                    maximized_x_j = milp.solution.get_objective_value()
+                    lb_j_plus_e = maximized_x_j + (input_values[j] - maximized_x_j) * 0.01
+                    input_vars[j].lb = lb_j_plus_e
+            bounds.append((lb_j_plus_e, ub_j_minus_e))
+
+        # ------------------  ASSERT  ------------------
+
+        for o in range(self.network.list_len_neurons[-1]):
+            if o == index_output_network:
+                continue
+
+            print(f'iteração: o={o}, true={index_output_network}', end='')
+
+            # add
+            output_constraint = milp.add_constraint(output_vars[o] >= output_vars[index_output_network], ctname=f'O_i>=O_j')
+            milp.solve()
+            if milp.solution is not None:
+                print("\nErro: isso não deveria acontecer!\n")
+                print("Layer x values:")
+                for neuron in input_values:
+                    print(neuron)
+                print("\nVariáveis:")
+                for var in milp.iter_continuous_vars():
+                    print(
+                        f"Nome: {var.name}, Tipo: {var.vartype}, Limite Inferior: {var.lb}, Limite Superior: {var.ub}")
+
+                # Exibir todas as restrições
+                print("\nRestrições:")
+                for constraint in milp.iter_constraints():
+                    print(f"{constraint}")
+                print("\nSolution:\n")
+                for var in milp.iter_variables():
+                    print(f"{var.name}: {milp.solution.get_value(var)}")
+                print("\n")
+
+            else:
+                print(" solution is None, OK")
+            milp.remove_constraint(output_constraint)
+
+        # ------------------  ASSERT  ------------------
+
+
+        return bounds
+
+    def _explain_intermediate_layer(self, index_previous_layer):
+        pass
+
+    def _explain_first_layer(self):
+        pass
